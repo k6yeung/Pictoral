b08787edd3d93a38d79d24f54438afd7d16a4a15
Kyle Yeung <k6yeung@uwaterloo.ca>
2017-01-17 20:15:32 -0500

    setup mongoDB session

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
+var MongoStore = require('connect-mongo')(session);
+
+
 
 // Database
 var mongo = require('mongodb');
@@ -15,6 +20,30 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
+
+//TODO: build mongo database connection url //
+
+var dbHost = process.env.DB_HOST || 'localhost';
+var dbPort = process.env.DB_PORT || 27017;
+var dbName = process.env.DB_NAME || 'Pictoral';
+
+var dbURL = 'mongodb://'+dbHost+':'+dbPort+'/'+dbName;
+if (app.get('env') == 'live'){
+// prepend url with authentication credentials //
+    dbURL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+dbHost+':'+dbPort+'/'+dbName;
+}
+
+app.use(session({
+        secret: 'faeb4453e5d14fe6f6d04637f78077c76c73d1b4',
+        proxy: true,
+        resave: true,
+        saveUninitialized: true,
+        store: new MongoStore({ url: dbURL })
+    })
+);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
