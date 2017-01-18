2573f1cbc073f658c7010c3d83952cc8b2bf9be0
Kyle Yeung <k6yeung@uwaterloo.ca>
2017-01-18 01:33:10 -0500

    AJAX form request

---
+++ public/javascripts/form-validator/loginValidator.js
@@ -0,0 +1,26 @@
+
+function LoginValidator()
+{
+// bind a simple alert window to this controller to display any errors //
+	this.loginErrors = $('.modal-alert');
+	
+	this.showLoginError = function(t, m)
+	{
+		$('.modal-alert .modal-header h4').text(t);
+		$('.modal-alert .modal-body').html(m);
+		this.loginErrors.modal('show');
+	}
+}
+
+LoginValidator.prototype.validateForm = function()
+{
+	if ($('#user').val() == ''){
+		this.showLoginError('Whoops!', 'Please enter a valid username');
+		return false;
+	}	else if ($('#pass').val() == ''){
+		this.showLoginError('Whoops!', 'Please enter a valid password');
+		return false;
+	}	else{
+		return true;
+	}
+}
