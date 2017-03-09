c7db66a8b964f3f0455430049c9a5962ff7d12e9
terranceyip <terrance161@gmail.com>
2017-03-08 21:50:43 -0500

    added commentments the accountValidator and account-manager class

--- public/javascripts/form-validator/accountValidator.js
+++ public/javascripts/form-validator/accountValidator.js
@@ -3,7 +3,7 @@ function AccountValidator()
 {
 // build array maps of the form inputs & control groups //
 
-	this.formFields = [$('#signup-user'), $('#signup-pass1'), $('#signup-pass2'), $('#signup-email')];
+	this.formFields = [$('#signup-user'), $('#signup-pass1'), $('#signup-pass2'), $('#signup-email')]; 
 	// this.controlGroups = [$('#name-cg'), $('#email-cg'), $('#user-cg'), $('#pass-cg')];
 	
 // bind the form-error modal window to this controller to display any errors //
@@ -11,28 +11,28 @@ function AccountValidator()
 	this.alert = $('.modal-form-errors');
 	this.alert.modal({ show : false, keyboard : true, backdrop : true});
 	
-	this.validateName = function(s)
+	this.validateName = function(s) //makes sure that the username is greater than 3 charaters
 	{
 		return s.length >= 3;
 	};
 	
-	this.validatePassword = function(s)
+	this.validatePassword = function(s) // makes sure that the password is longer than 6 charaters
 	{
 		return s.length >= 6;
 	};
 
-	this.validateSecondPassword = function(s,e) {
+	this.validateSecondPassword = function(s,e) { //makes sure that the password matches the previous password that was entered
 		return s === e;
 	}
 	
-	this.validateEmail = function(e)
+	this.validateEmail = function(e) 
 	{
 		// var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		// return re.test(e);
 		return true;
 	};
 	
-	this.showErrors = function(a)
+	this.showErrors = function(a) //shows error messages when an error occurs
 	{
 		$('.modal-form-errors .modal-body p').text('Please correct the following problems :');
 		var ul = $('.modal-form-errors .modal-body ul');
@@ -43,34 +43,34 @@ function AccountValidator()
 
 }
 
-AccountValidator.prototype.showInvalidEmail = function()
+AccountValidator.prototype.showInvalidEmail = function() //displays an error message to the user when the email is invalid 
 {
 	// this.controlGroups[1].addClass('error');
 	this.showErrors(['That email address is already in use.']);
 }
 
-AccountValidator.prototype.showInvalidUserName = function()
+AccountValidator.prototype.showInvalidUserName = function() //displays an error message to the user when the username is invalid
 {
 	// this.controlGroups[2].addClass('error');
 	this.showErrors(['That username is already in use.']);
 }
 
-AccountValidator.prototype.validateForm = function()
+AccountValidator.prototype.validateForm = function()  //this are all of the other errors that can occur
 {
 	var e = [];
 	// for (var i=0; i < this.controlGroups.length; i++) this.controlGroups[i].removeClass('error');
-    if (this.validateName(this.formFields[0].val()) == false) {
+    if (this.validateName(this.formFields[0].val()) == false) { //when username is not 3 character or more
         // this.controlGroups[2].addClass('error');
         e.push('Username Should Be At Least 3 Characters');
     }
-	if (this.validatePassword(this.formFields[1].val()) == false) {
+	if (this.validatePassword(this.formFields[1].val()) == false) { //when password is less than 6 characters
 		// this.controlGroups[3].addClass('error');
 		e.push('Password Should Be At Least 6 Characters');
 	}
-	if (this.validateSecondPassword(this.formFields[1].val(), this.formFields[2].val()) == false){
+	if (this.validateSecondPassword(this.formFields[1].val(), this.formFields[2].val()) == false){ //when the retyped password does not match the password entered previously
 		e.push('Both Passwords Do Not Match');
 	}
-    if (this.validateEmail(this.formFields[3].val()) == false) {
+    if (this.validateEmail(this.formFields[3].val()) == false) { 
         // this.controlGroups[1].addClass('error');
         e.push('Please Enter A Valid Email');
     }
--- server/account-manager.js
+++ server/account-manager.js
@@ -8,9 +8,19 @@ var moment 		= require('moment');
 	Establish DB connection
 */
 
-var dbName = process.env.DB_NAME || 'PictoralDB';
-var dbHost = process.env.DB_HOST || 'localhost';
-var dbPort = process.env.DB_PORT || 27017;
+var dbName = process.env.DB_NAME || 'PictoralDB'; // setting the database name
+var dbHost = process.env.DB_HOST || 'localhost';  // database host
+var dbPort = process.env.DB_PORT || 27017; 		  // database port
+
+ /**
+
+        Creates a new database
+
+         * @param dbHost is the database host
+
+         * @param dbPort is the port that the database is using
+
+         */
 
 var db = new MongoDB(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
 db.open(function(e, d){
@@ -20,21 +30,29 @@ db.open(function(e, d){
 		if (process.env.NODE_ENV == 'live') {
 			db.authenticate(process.env.DB_USER, process.env.DB_PASS, function(e, res) {
 				if (e) {
-					console.log('mongo :: error: not authenticated', e);
+					console.log('mongo :: error: not authenticated', e); //if there is an error connecting to the DB
 				}
 				else {
-					console.log('mongo :: authenticated and connected to database :: "'+dbName+'"');
+					console.log('mongo :: authenticated and connected to database :: "'+dbName+'"'); // DB is connected
 				}
 			});
 		} else {
-			console.log('mongo :: connected to database :: "'+dbName+'"');
+			console.log('mongo :: connected to database :: "'+dbName+'"'); //If DB is already connected
 		}
 	}
 });
 
 var accounts = db.collection('accounts');
 
-/* login validation methods */
+ /**
+
+         Used to Validate Login Automatically
+
+         * @param user username of the account
+
+         * @param pass password of the account
+
+         */
 
 exports.autoLogin = function(user, pass, callback)
 {
@@ -47,14 +65,25 @@ exports.autoLogin = function(user, pass, callback)
 	});
 }
 
+/**
+
+         Used to Validate Login Manually 
+
+         * @param user username that the user enters
+
+         * @param pass password that the user enters
+		 
+
+         */
+
 exports.manualLogin = function(user, pass, callback)
 {
-	accounts.findOne({user:user}, function(e, o) {
-		if (o == null){
+	accounts.findOne({user:user}, function(e, o) { 
+		if (o == null){						// user name cannot be found in the DB
 			callback('user-not-found');
 		}	else{
 			validatePassword(pass, o.pass, function(err, res) {
-				if (res){
+				if (res){					// password does not match
 					callback(null, o);
 				}	else{
 					callback('invalid-password');
@@ -64,14 +93,20 @@ exports.manualLogin = function(user, pass, callback)
 	});
 }
 
-/* record insertion, update & deletion methods */
+/**
+
+         Used for adding new Accounts
+
+         * @param newData used to specify that this is new data that is being inputted into the DB
+
+         */
 
 exports.addNewAccount = function(newData, callback)
 {
 	accounts.findOne({user:newData.user}, function(e, o) {
-		if (o){
+		if (o){								// user name is already on the DB and therefore invalid
 			callback('username-taken');
-		}	else{
+		}	else{							// hash the password and create a timestamp of when it was created
 			saltAndHash(newData.pass, function(hash){
 				newData.pass = hash;
 				// append date stamp when record was created //
@@ -83,7 +118,11 @@ exports.addNewAccount = function(newData, callback)
 	});
 }
 
-/* private encryption & validation methods */
+/**
+
+         Generate a salt that is used for password validation
+
+         */
 
 var generateSalt = function()
 {
@@ -95,17 +134,36 @@ var generateSalt = function()
 	}
 	return salt;
 }
+/**
+
+         Creating a hash for encryption 
+		 
+         */
 
 var md5 = function(str) {
 	return crypto.createHash('md5').update(str).digest('hex');
 }
+/**
 
+         Use both the salt and hash for passwords
+
+         * @param pass user's password
+
+         */
 var saltAndHash = function(pass, callback)
 {
 	var salt = generateSalt();
 	callback(salt + md5(pass + salt));
 }
+/**
 
+         validate the password after the hash + salt 
+
+         * @param plainPass user's password
+
+         * @param hashedPass password that has been hashed
+
+         */
 var validatePassword = function(plainPass, hashedPass, callback)
 {
 	var salt = hashedPass.substr(0, 10);
